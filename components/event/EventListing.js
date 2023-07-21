10c9e16c50536b267177c832dc5214bc76d71516
bertrandsimon <trandber@hotmail.com>
2023-07-21 13:58:46 +0200

    Events listing done

--- components/event/EventListing.js
+++ components/event/EventListing.js
@@ -2,8 +2,18 @@ import styles from '../../styles/EventListing.module.css';
 
 import { useState, useEffect } from 'react';
 
-import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import ModeIcon from '@mui/icons-material/Mode';
 
 function EventListing() {
 
@@ -14,20 +24,75 @@ useEffect(() => {
   .then(response => response.json())
   .then(data => {
     setEventsData(data.all)
-    console.log(data)
-    console.log(data.all)
-    //console.log('eventsData : ', eventsData)
   })
 }, []);
 
 useEffect(() => {
-  console.log('eventsData:', eventsData);
+  //console.log('eventsData:', eventsData);
 }, [eventsData]);
 
 
+const handleDelete = (id) => {
+  console.log(id)
+  fetch(`http://localhost:3000/events/delete/${id}`, {
+    method: 'DELETE',
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Assuming the API returns the updated events list after deletion
+      console.log('DELETED')
+      setEventsData(prevEvents => prevEvents.filter(event => event._id !== id));
+    })
+    .catch(error => {
+      console.error('Error deleting event:', error);
+    });
+};
   return (
     <div className={styles.container}>
-      <h1>Events listing</h1>
+      <h1>Listing courses</h1>
+    
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      
+      
+
+        <TableHead>
+          <TableRow>
+            <TableCell>NOM</TableCell>
+            <TableCell align="center">EMPLACEMENT</TableCell>
+            <TableCell align="center">MODIFIER</TableCell>
+            <TableCell align="center">SUPPRIMER</TableCell>
+            
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {eventsData.map((event) => (
+            <TableRow
+              key={event.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" className='up'>
+                {event.name}
+              </TableCell>
+            
+              <TableCell align="center" >  {event.location}</TableCell>
+              <TableCell align="center">
+                <IconButton style={{ color: 'white' }} aria-label="Modifier">
+                    <ModeIcon/>
+                </IconButton>
+              </TableCell>
+              <TableCell align="center"> 
+                <IconButton style={{ color: 'white' }} aria-label="Supprimer" onClick ={ () => handleDelete(event._id) }>
+                  <DeleteForeverIcon/>
+                </IconButton>
+                </TableCell>
+         
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   );
 
